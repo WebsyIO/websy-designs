799127eee31bc74e2ff8f56ef9580f3566079a26
Greg Munro <gregmunro@Websy-Greg.local>
2022-06-15 10:32:42 +0200

    prev and next success

--- dist/websy-designs.debug.js
+++ dist/websy-designs.debug.js
@@ -211,9 +211,9 @@ class WebsyCarousel {
       console.log('No element Id provided')
     }
     const el = document.getElementById(elementId)
-    el.addEventListener('click', this.handleClick.bind(this))
     if (el) {
       this.elementId = elementId
+      el.addEventListener('click', this.handleClick.bind(this))
       this.render()
     }
   }
@@ -229,7 +229,7 @@ class WebsyCarousel {
 
   next () {
     document.getElementById(`${this.elementId}_frame_${this.options.currentFrame}`)
-      .style.transform = `translateX(-100%);`
+      .style.transform = `translateX(-100%)`
   }
 
   play () {
@@ -305,12 +305,12 @@ class WebsyCarousel {
       <svg xmlns="http://www.w3.org/2000/svg" class="websy-prev-arrow"
       viewBox="0 0 512 512">
       <title>Caret Back</title>
-      <path id="websy-prev-arrow" d="M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z"/>
+      <path d="M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z"/>
       </svg>
       </div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="websy-next-arrow">
       <title>Caret Forward</title>
-      <path id="websy-next-arrow" d="M190.06 414l163.12-139.78a24 24 0 000-36.44L190.06 98c-15.57-13.34-39.62-2.28-39.62 18.22v279.6c0 20.5 24.05 31.56 39.62 18.18z"/>
+      <path d="M190.06 414l163.12-139.78a24 24 0 000-36.44L190.06 98c-15.57-13.34-39.62-2.28-39.62 18.22v279.6c0 20.5 24.05 31.56 39.62 18.18z"/>
       </svg>
       `
       }
--- dist/websy-designs.js
+++ dist/websy-designs.js
@@ -305,10 +305,10 @@ var WebsyCarousel = /*#__PURE__*/function () {
     }
 
     var el = document.getElementById(elementId);
-    el.addEventListener('click', this.handleClick.bind(this));
 
     if (el) {
       this.elementId = elementId;
+      el.addEventListener('click', this.handleClick.bind(this));
       this.render();
     }
   }
@@ -327,7 +327,7 @@ var WebsyCarousel = /*#__PURE__*/function () {
   }, {
     key: "next",
     value: function next() {
-      document.getElementById("".concat(this.elementId, "_frame_").concat(this.options.currentFrame)).style.transform = "translateX(-100%);";
+      document.getElementById("".concat(this.elementId, "_frame_").concat(this.options.currentFrame)).style.transform = "translateX(-100%)";
     }
   }, {
     key: "play",
@@ -394,7 +394,7 @@ var WebsyCarousel = /*#__PURE__*/function () {
         });
 
         if (this.options.showPrevNext === true) {
-          html += "\n      <svg xmlns=\"http://www.w3.org/2000/svg\" class=\"websy-prev-arrow\"\n      viewBox=\"0 0 512 512\">\n      <title>Caret Back</title>\n      <path id=\"websy-prev-arrow\" d=\"M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z\"/>\n      </svg>\n      </div>\n      <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\" class=\"websy-next-arrow\">\n      <title>Caret Forward</title>\n      <path id=\"websy-next-arrow\" d=\"M190.06 414l163.12-139.78a24 24 0 000-36.44L190.06 98c-15.57-13.34-39.62-2.28-39.62 18.22v279.6c0 20.5 24.05 31.56 39.62 18.18z\"/>\n      </svg>\n      ";
+          html += "\n      <svg xmlns=\"http://www.w3.org/2000/svg\" class=\"websy-prev-arrow\"\n      viewBox=\"0 0 512 512\">\n      <title>Caret Back</title>\n      <path d=\"M321.94 98L158.82 237.78a24 24 0 000 36.44L321.94 414c15.57 13.34 39.62 2.28 39.62-18.22v-279.6c0-20.5-24.05-31.56-39.62-18.18z\"/>\n      </svg>\n      </div>\n      <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\" class=\"websy-next-arrow\">\n      <title>Caret Forward</title>\n      <path d=\"M190.06 414l163.12-139.78a24 24 0 000-36.44L190.06 98c-15.57-13.34-39.62-2.28-39.62 18.22v279.6c0 20.5 24.05 31.56 39.62 18.18z\"/>\n      </svg>\n      ";
         }
 
         html += "\n      </div>\n      ";
